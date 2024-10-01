8a015920bf491f5094523f8fa126683eafeb7d63
ivan <ivantaiwan4@gmail.com>
2024-10-02 03:29:24 +0800

    fix to render platform v1.7

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config(); // 載入環境變數
 const app = express();
 
 app.use(cors({
-  origin: 'https://golf-frontend.onrender.com' // 替換為你前端的實際域名
+  origin: 'https://golfweb-frontend.onrender.com' // 替換為你前端的實際域名
   //credentials: true, // 如果需要處理身份驗證或cookie
 }));
 app.use(bodyParser.json());
